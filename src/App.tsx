00b007249f219ae7bd7d1c9f47c8c016f5df8e25
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2024-08-23 20:14:33 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,72 @@
+import { useState, useEffect } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import "./App.css";
+import { MouseEventHandler } from "react";
+
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 function App() {
+  const { watch, register, handleSubmit, reset } = useForm<FormData>();
+
+  const [disableCancelBtn, setDisableCancelBtn] = useState<boolean>(true);
+  console.log(disableCancelBtn);
+
+  const [name, email, password] = watch(["name", "email", "password"]);
+
+  useEffect(() => {
+    if (name || email || password) {
+      setDisableCancelBtn(false);
+    } else {
+      setDisableCancelBtn(true);
+    }
+  }, [name, email, password]);
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  const handleCancel: MouseEventHandler<HTMLButtonElement> = () => {
+    reset();
+    setDisableCancelBtn(true);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="labelForm">
         <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" placeholder="name" {...register("name")} />
       </div>
       <div className="labelForm">
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          {...register("email")}
+        />
       </div>
       <div className="labelForm">
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          {...register("password")}
+        />
       </div>
       <div>
         <button type="submit">Submit</button>
-        <button type="button">Cancel</button>
+        <button
+          type="button"
+          onClick={handleCancel}
+          disabled={disableCancelBtn}
+        >
+          Cancel
+        </button>
       </div>
     </form>
   );
